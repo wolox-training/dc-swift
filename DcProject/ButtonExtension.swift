544e14eedf2fa41174b1eee2986d76572dfbdef7
Daniel Charry <daniel.charry@wolox.co>
2018-11-27 15:21:34 -0500

    Archivo de extension de los botones principales
    Aspecto grafico round: border redondos y gradient: para gradientes de background del botón

---
+++ DcProject/ButtonExtension.swift
@@ -0,0 +1,37 @@
+//
+//  ButtonExtension.swift
+//  DcProject
+//
+//  Created by Daniel Andres Charry on 27/11/18.
+//  Copyright © 2018 Wolox. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    
+    //Round borders
+    func round(color: UIColor? = nil, border: Double, radius: Double? = 2.0){
+        
+        layer.cornerRadius = bounds.height / CGFloat(radius!)
+        layer.borderWidth = CGFloat(border)
+        layer.borderColor = color?.cgColor
+        clipsToBounds = true
+    }
+    
+    // Gradient style
+    func gradient(colors: [UIColor]? = nil, border: Double, radius: Double? = 2.0){
+        let gradient = CAGradientLayer()
+        gradient.frame = CGRect(x: 0, y: 0, width: 275, height: 44)
+        gradient.colors = (colors != nil) ? colors : [
+            UIColor(red:0, green:0.68, blue:0.93, alpha:1).cgColor,
+            UIColor(red:0.22, green:0.8, blue:0.8, alpha:1).cgColor
+        ]
+        gradient.locations = [0, 1]
+        gradient.startPoint = CGPoint(x: 0.06, y: 0.5)
+        gradient.endPoint = CGPoint(x: 1, y: 0.5)
+        round( border: border, radius: radius)
+        layer.addSublayer(gradient)
+    }
+    
+}
