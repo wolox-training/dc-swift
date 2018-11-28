e7c83c07e62d2c0a0c8acbe3fdf63d7e42518202
Daniel Charry <daniel.charry@wolox.co>
2018-11-28 16:32:23 -0500

    Modelo inicial de un comentario

---
+++ DcProject/Comment.swift
@@ -0,0 +1,19 @@
+//
+//  Comment.swift
+//  DcProject
+//
+//  Created by Daniel Andres Charry on 28/11/18.
+//  Copyright © 2018 Wolox. All rights reserved.
+//
+
+import Foundation
+import Argo
+import Runes
+import Curry
+import Networking
+
+public struct Comment {
+    
+    let content: String
+    
+}
