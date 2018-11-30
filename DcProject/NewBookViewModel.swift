31923df9e3254939bde58994caa42394857ad6bf
Daniel Charry <daniel.charry@wolox.co>
2018-11-30 14:37:20 -0500

    Model de NewBook

---
+++ DcProject/NewBookViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  NewBookViewModel.swift
+//  DcProject
+//
+//  Created by Daniel Andres Charry on 30/11/18.
+//  Copyright © 2018 Wolox. All rights reserved.
+//
+
+import Foundation
+
+final class NewBookViewModel {
+    
+    //  private let _repository: <#RepositoryDependency#>
+    
+    //  init(repository: <#RepositoryDependency#>) {
+    //    _repository = repository
+    //  }
+    init() {
+        
+    }
+    
+}
