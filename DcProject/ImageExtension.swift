8b1fce824fd801b52f8c75c312b319cbeb9220c9
Daniel Charry <daniel.charry@wolox.co>
2018-12-03 16:29:30 -0500

    Creacion de extension de UIImageView

---
+++ DcProject/ImageExtension.swift
@@ -0,0 +1,23 @@
+//
+//  ImageExtension.swift
+//  DcProject
+//
+//  Created by Daniel Andres Charry on 3/12/18.
+//  Copyright © 2018 Wolox. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+ 
+    func round(colorBorder: UIColor? = nil, colorBackground: UIColor? = nil, border: Double, radius: Double){
+        
+        layer.cornerRadius = frame.size.width / CGFloat(radius)
+        layer.masksToBounds = true
+        layer.borderWidth = CGFloat(border)
+        layer.borderColor = colorBorder?.cgColor
+        layer.backgroundColor = colorBackground?.cgColor
+        
+    }
+    
+}
