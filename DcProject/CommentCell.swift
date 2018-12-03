ff918709d16b97608eba62619a27dd5c6ab6fd40
Daniel Charry <daniel.charry@wolox.co>
2018-12-03 16:31:03 -0500

    Imagen de los nombres de los username

--- DcProject/CommentCell.swift
+++ DcProject/CommentCell.swift
@@ -18,9 +18,22 @@ public class CommentCell: UITableViewCell, NibLoadable {
     @IBOutlet weak var commentText: UILabel!
     
     func configCell(comment: Comment) {
+        
+        let username = comment.user.username
+        let startIndex = username.index(username.startIndex, offsetBy: 0)
+        let endIndex = username.index(username.startIndex, offsetBy: 1)
+        let letter = String(username[startIndex...endIndex]).capitalized
+        
+        setUpComment()
         commentText.text = comment.content
         commentName.text = comment.user.username
-        commentImage.image = #imageLiteral(resourceName: "user")
+        commentImage.image = letter.toImage(fontSize: 20.0)
+    }
+    
+    func setUpComment() {
+        
+        commentImage.round(colorBorder: UIColor(red: 0, green: 0.68, blue: 0.93, alpha: 1), border: 1.0, radius: 2.0)
+        
     }
     
 }
