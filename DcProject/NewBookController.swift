75ab8417545cb83cc4ca633b5813c7ac50cdb80c
Daniel Charry <daniel.charry@wolox.co>
2018-12-06 13:56:30 -0500

    Se crea toda la funcionalidad de los textField y los botones

--- DcProject/NewBookController.swift
+++ DcProject/NewBookController.swift
@@ -8,8 +8,9 @@
 
 import Foundation
 import UIKit
+import WolmoCore
 
-final class NewBookController: UIViewController {
+final class NewBookController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     private let _viewModel: NewBookViewModel
     private let _view: NewBookView = NewBookView.loadFromNib()!
@@ -35,21 +36,83 @@ final class NewBookController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        bindViewModel()
         confingNavBar()
+        configButton()
+        _view.newBookName.delegate = self
+        _view.newBookAuthor.delegate = self
+        _view.newBookYear.delegate = self
+        _view.newBookTopic.delegate = self
+        _view.newBookDescription.delegate = self
         
     }
+    
     func confingNavBar() {
         navigationItem.title = "NAVBAR-TITLE-ADDNEW".localized()
     }
     
-}
-
-// MARK: - VM binding
-private extension NewBookController {
+    func configButton() {
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tap))
+        tapGesture.numberOfTapsRequired = 1
+        _view.btnSubmit.addGestureRecognizer(tapGesture)
+        
+        let tapImage = UITapGestureRecognizer(target: self, action: #selector(setImage))
+        tapImage.numberOfTapsRequired = 1
+        _view.btnAddImage.addGestureRecognizer(tapImage)
+    }
     
-    private func bindViewModel() {
+    @objc func setImage(sender: UITapGestureRecognizer) {
+        
+        let imag = UIImagePickerController()
+        imag.delegate = self
+        imag.sourceType = .photoLibrary
+        imag.allowsEditing = true
+        
+        present(imag, animated: true)
         
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let image = info["UIImagePickerControllerOriginalImage"] as? UIImage else {
+            NSLog("No image found")
+            return
+        }
+        _view.btnAddImage.setImage(image, for: .normal)
+        picker.dismiss(animated: true)
+    }
+    
+    @objc func tap(sender: UITapGestureRecognizer) {
+        
+        let book = Book.init(
+            title: _view.newBookName.text!,
+            author: _view.newBookAuthor.text!,
+            image: NewBookController.MockImage,
+            id: NewBookController.MockId,
+            year: _view.newBookYear.text!,
+            genre: NewBookController.MockGenre)
+        
+        _viewModel.newBook = book
+        
+        _viewModel.postBook().responseJSON {[unowned self] _ in
+            self.presentAlert(ErrorAlertViewModel(title: "CREATION-BOOK-SUCCESSFUL".localized(),
+                                                  message: book.title,
+                                                  dismissButtonTitle: "BUTTON-ALERT-OK".localized()))
+        }
+    }
+    
+}
+
+extension NewBookController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        _view.endEditing(true)
+        return true
+    }
+}
+
+// MARK: - Constants
+private extension NewBookController {
+    static let MockImage: String = "https://read.macmillan.com/wp-content/uploads/2016/04/book-cover-placeholder.png"
+    static let MockGenre: String = "Terror"
+    static let MockId: Int = 1
 }
