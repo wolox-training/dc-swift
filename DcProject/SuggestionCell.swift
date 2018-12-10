7282704021785373b55e6f73602f337558775710
Daniel Charry <daniel.charry@wolox.co>
2018-12-10 16:26:20 -0500

    Creación de SuggestionCell, nueva validacion del url de la imagen para cuando el url no sea valido

---
+++ DcProject/SuggestionCell.swift
@@ -0,0 +1,38 @@
+//
+//  SuggestionCell.swift
+//  DcProject
+//
+//  Created by Daniel Andres Charry on 10/12/18.
+//  Copyright © 2018 Wolox. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import WolmoCore
+import Kingfisher
+
+public class SuggestionCell: UICollectionViewCell, NibLoadable {
+    
+    @IBOutlet weak var suggestionImage: UIImageView!
+    
+    func confingCell(suggestion: Suggestion) {
+        
+        if let url = URL(string: suggestion.link) {
+            
+            if UIApplication.shared.canOpenURL(url) {
+                
+                let resource = ImageResource(downloadURL: url)
+                suggestionImage.kf.indicatorType = .activity
+                suggestionImage.kf.setImage(with: resource, options: [.transition(ImageTransition.fade(1))])
+                
+            } else {
+                
+                suggestionImage.image = #imageLiteral(resourceName: "book")
+            }
+            
+        } else {
+            
+            suggestionImage.image = #imageLiteral(resourceName: "book")
+        }
+    }
+}
