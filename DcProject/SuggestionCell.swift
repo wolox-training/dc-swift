02f01c52a6ffdc3298feee3f470620bd1fd6132b
Daniel Charry <daniel.charry@wolox.co>
2018-12-11 10:50:02 -0500

    Correción de espacios

--- DcProject/SuggestionCell.swift
+++ DcProject/SuggestionCell.swift
@@ -16,22 +16,15 @@ public class SuggestionCell: UICollectionViewCell, NibLoadable {
     @IBOutlet weak var suggestionImage: UIImageView!
     
     func confingCell(suggestion: Suggestion) {
-        
         if let url = URL(string: suggestion.link) {
-            
             if UIApplication.shared.canOpenURL(url) {
-                
                 let resource = ImageResource(downloadURL: url)
                 suggestionImage.kf.indicatorType = .activity
                 suggestionImage.kf.setImage(with: resource, options: [.transition(ImageTransition.fade(1))])
-                
             } else {
-                
                 suggestionImage.image = #imageLiteral(resourceName: "book")
             }
-            
         } else {
-            
             suggestionImage.image = #imageLiteral(resourceName: "book")
         }
     }
